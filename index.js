5381b09afdbbebaafd5ad2cf7dad72def220cce1
iamsadikrahman <imasadikrahman@outlook.com>
2023-07-31 00:02:53 +0600

    json data added and server added

--- index.js
+++ index.js
@@ -1,10 +1,44 @@
-import express from "express";
+// import express from "express";
+
+// const app = express();
+// const port = 9000;
+// app.use("/", (req, res) => {
+//   res.json({ message: "Hello From Express App" });
+// });
+
+// app.listen(9000, () => {
+//   console.log(`Starting Server on Port ${port}`);
+// });
+
+
+
+const express = require('express');
+const data = require('./data.json');
+
+console.log(data);
 
 const app = express();
-const port = 9000;
-app.use("/", (req, res) => {
-  res.json({ message: "Hello From Express App" });
+
+app.get('/movies', (req, res) => {
+  res.json(data);
 });
+
+// Define a route to get data by ID
+app.get('/movies/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  const dataItem = data.find(item => item.id === id);
+  if (dataItem) {
+    res.json(dataItem);
+  } else {
+    res.status(404).json({ message: 'Data not found' });
+  }
+});
+
+// Root route
+app.get('/', (req, res) => {
+  res.send('Welcome to the Movie API!');
+});
+
 
 app.listen(9000, () => {
   console.log(`Starting Server on Port ${port}`);
